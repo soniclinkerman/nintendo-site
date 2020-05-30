8c6e22d5adb3a5cd618cf86bb3e9f2876e36aa70
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-05-30 18:25:12 -0400

    Change css name in Trending

--- src/Trending/Trending.js
+++ src/Trending/Trending.js
@@ -26,7 +26,7 @@ const Trending = () => {
                             <div className="trending-comp col">
                                 <a className="product" href="">
                                 <img src={ac}/>
-                                <a href="#" className="title">Animal Crossing<span>TM</span>:<br/> New Horizon</a>
+                                <a href="#" className="trending-title">Animal Crossing<span>TM</span>:<br/> New Horizon</a>
                             
                                 <p className="date">Released: 03/20/2020</p>
                                 <p className="price">$59.99</p>
@@ -37,7 +37,7 @@ const Trending = () => {
                             <div className="trending-comp special col">
                                 <a className="product" href="">
                                 <img src={minecraft_dungeons}/>
-                                <a href="#" className="title">Minecraft Dungeons</a>
+                                <a href="#" className="trending-title">Minecraft Dungeons</a>
                             
                                 <p className="date">Released: 05/26/2020</p>
                                 <p className="price">$19.99</p>
@@ -50,7 +50,7 @@ const Trending = () => {
                         <div className="trending-comp col">
                                 <a className="product" href="">
                                 <img src={xenoblade}/>
-                                <a href="#" className="title">Xenoblade Chronicles<span>TM</span>:<br/> Definitive Edition</a>
+                                <a href="#" className="trending-title">Xenoblade Chronicles<span>TM</span>:<br/> Definitive Edition</a>
                             
                                 <p className="date">Released: 05/29/2020</p>
                                 <p className="price">$59.99</p>
@@ -63,7 +63,7 @@ const Trending = () => {
                             <div className="trending-comp col">
                                 <a className="product" href="">
                                 <img src={m_s}/>
-                                <a href="#" className="title">Mario & Sonic at the<br/> Olympic Games<br/>Tokyo 2020</a>
+                                <a href="#" className="trending-title">Mario & Sonic at the<br/> Olympic Games<br/>Tokyo 2020</a>
                             
                                 <p className="date">Released: 03/20/2020</p>
                                 <p className="price">$59.99</p>
